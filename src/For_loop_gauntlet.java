751059ebfd72b56687c73d9f0a6e61a93fa9e839
GT <league@wts14-4.attlocal.net>
2019-01-09 19:57:24 -0800

    for loop gauntlet circle ring goofynames

---
+++ src/For_loop_gauntlet.java
@@ -0,0 +1,17 @@
+
+public class For_loop_gauntlet {
+
+	// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			System.out.println(i);
+		}
+		for (int i = 100; i > 0; i--) {
+			System.out.println(i);
+
+		}
+		if (i % 2 == 0) {
+
+		}
+	}
+}
--- src/nested_loops/CircleRing.java
+++ src/nested_loops/CircleRing.java
@@ -7,29 +7,38 @@ package nested_loops;
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		// 1. Make a new Robot
+		Robot a = new Robot();
 
+		// 2. Set your robot’s position to x=150 and y=200
+		a.moveTo(150, 200);
+		// 3. Put the robot's pen down
+		a.penDown();
+		// 4. Set the robot’s speed to 10
+		a.setSpeed(100);
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 360; i++) {
+			a.move(3);
+			a.turn(1);
+			if (i % 20 == 0) {
+				for (int j = 0; j < 360; j++) {
+					a.move(1);
+					a.turn(1);
+				}
+			}
+		}
+		// 6. Move the robot 3 pixels
 
-       //  2. Set your robot’s position to x=150 and y=200
+		// 7. Turn the robot 1 degree
 
-        // 3. Put the robot's pen down
-
-        // 4. Set the robot’s speed to 10
-
-        // 5. Do everything below here 360 times (use i as the counter)
-
-	        // 6. Move the robot 3 pixels
-
-       //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
+		// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
 
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
-				// 10. Move the robot 1 pixel
+		// 10. Move the robot 1 pixel
 
-				// 11. Turn the robot 1 degree
-}
+		// 11. Turn the robot 1 degree
+	}
 }
